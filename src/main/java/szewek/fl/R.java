fc3c64cabcb163044f03bb392da11d0fc012e8f6
Szewek <shevkpl@gmail.com>
2017-06-25 14:35:05 +0200

    JEI dependency is not required

--- src/main/java/szewek/fl/R.java
+++ src/main/java/szewek/fl/R.java
@@ -4,7 +4,7 @@ public final class R {
 	public static final String
 			FL_ID = "fl",
 			FL_VERSION = "${version}",
-			FL_DEPS = "required-after:jei@[4.3.0,)";
+			FL_DEPS = "after:jei@[4.3.0,)";
 
 	private R() {}
 }
