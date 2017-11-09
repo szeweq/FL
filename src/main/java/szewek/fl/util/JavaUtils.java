b67d4f2bdb0a46d749e732e9ffee302097e3d2d6
Szewek <shevkpl@gmail.com>
2017-11-09 14:16:44 +0100

    ASM check function

--- src/main/java/szewek/fl/util/JavaUtils.java
+++ src/main/java/szewek/fl/util/JavaUtils.java
@@ -1,7 +1,12 @@
 package szewek.fl.util;
 
+import net.minecraftforge.fml.common.discovery.ASMDataTable;
+
 import javax.annotation.Nullable;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.Set;
+import java.util.function.BiConsumer;
 
 public final class JavaUtils {
 
@@ -28,6 +33,18 @@ public final class JavaUtils {
 		for (int i = 0, l = t.length; i < l; i++)
 			t[i] = fill;
 		return t;
+	}
+
+	public static <A extends Annotation> void eachAnnotatedClasses(ASMDataTable asdt, Class<A> ac, BiConsumer<A, Class<?>> eachfn) {
+		final Set<ASMDataTable.ASMData> aset = asdt.getAll(ac.getCanonicalName());
+		for (ASMDataTable.ASMData data : aset) {
+			final String cn = data.getClassName();
+			if (!cn.equals(data.getObjectName())) continue;
+			final Class<?> c = getClassSafely(cn);
+			if (c == null) continue;
+			final A a = c.getAnnotation(ac);
+			eachfn.accept(a, c);
+		}
 	}
 
 	private JavaUtils() {}
