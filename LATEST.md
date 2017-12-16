1dfe4d66624f3d7326cb8dad77202547935d8dcf
Szewek <shevkpl@gmail.com>
2017-12-16 18:13:55 +0100

    New FLCloud API

--- LATEST.md
+++ LATEST.md
@@ -1,5 +1,2 @@
-# 2.10.0 (Can't do 3.0.0 – it is reserved for MC 1.13)
-- New logo
-- New Rollbar API
-- FluxWork API moved from Minecraft-Flux
-- Easier item and block registration
+# 2.11.0
+- New FLCloud API
--- src/main/java/szewek/fl/network/FLCloud.java
+++ src/main/java/szewek/fl/network/FLCloud.java
@@ -1,20 +1,27 @@
 package szewek.fl.network;
 
 import com.google.common.io.ByteStreams;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 import net.minecraftforge.fml.common.Loader;
 import szewek.fl.FL;
 
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.google.gson.internal.bind.TypeAdapters.JSON_ELEMENT;
+
 public class FLCloud {
 	private static Map<String, FLCloud> apis = Collections.synchronizedMap(new HashMap<>());
 
 	private final String name, key;
-	private String cachedVersion = "";
+	private String clikey = null, cachedVersion = "";
 
 	private FLCloud(String n, String k) {
 		name = n;
@@ -26,15 +33,17 @@ public class FLCloud {
 		return 31 * name.hashCode() + key.hashCode();
 	}
 
-	private HttpURLConnection connect(String path) {
-		HttpURLConnection huc = null;
+	public APICall connect(String path) {
 		try {
-			huc = (HttpURLConnection) new URL("https", "cloudflux.herokuapp.com", 443, path, null).openConnection();
-			huc.addRequestProperty("Authorization", "Bearer " + key);
+			final HttpURLConnection huc = (HttpURLConnection) new URL("https", "api-fl.1d35.starter-us-east-1.openshiftapps.com", 443, path, null).openConnection();
+			huc.setRequestProperty("Authorization", "Bearer " + key);
+			if (clikey != null)
+				huc.setRequestProperty("X-Client-Key", clikey);
+			return new APICall(huc);
 		} catch (Exception x) {
 			x.printStackTrace();
 		}
-		return huc;
+		return null;
 	}
 
 	public static FLCloud getAPI(String n, String k) {
@@ -47,24 +56,79 @@ public class FLCloud {
 	}
 
 	public static void checkVersions() {
-		HttpURLConnection huc;
+		APICall ac;
 		for (FLCloud api : apis.values()) {
-			huc = api.connect("/api/version/" + Loader.MC_VERSION);
-			if (huc == null) {
+			ac = api.connect("/api/version/" + Loader.MC_VERSION);
+			if (ac == null) {
 				continue;
 			}
 			try {
-				api.cachedVersion = new String(ByteStreams.toByteArray(huc.getInputStream()), "UTF-8");
-				FL.L.info("Cached version for {}: {}", api.name, api.cachedVersion);
-			} catch (Exception x) {
-				String es = "";
-				try {
-					es = new String(ByteStreams.toByteArray(huc.getErrorStream()), "UTF-8");
-				} catch (Exception x2) {
-					x2.printStackTrace();
+				final String es = ac.responseText();
+				if (ac.statusCode() == 200) {
+					api.cachedVersion = es;
+					FL.L.info("Cached version for {}: {}", api.name, api.cachedVersion);
+				} else {
+					FL.L.error("Got Error Response: {}", es);
 				}
-				FL.L.error(es, x);
+
+			} catch (Exception x) {
+				FL.L.error("Exception thrown while getting a version for " + api.name, x);
 			}
+		}
+	}
+
+	public static class APICall {
+		private final HttpURLConnection conn;
+
+		private APICall(HttpURLConnection huc) {
+			conn = huc;
+		}
+
+		public int statusCode() throws IOException {
+			return conn.getResponseCode();
+		}
+
+		private InputStream getResponse() throws IOException {
+			final int rc = conn.getResponseCode();
+			return rc / 100 == 2 ? conn.getInputStream() : conn.getErrorStream();
+		}
+
+		public APICall postJSON(JsonElement o) throws IOException {
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setDoOutput(true);
+			JsonWriter jw = new JsonWriter(new BufferedWriter(new OutputStreamWriter(conn.getOutputStream())));
+			jw.setLenient(true);
+			JSON_ELEMENT.write(jw, o);
+			jw.close();
+			conn.getOutputStream().close();
+			return this;
+		}
+
+		public JsonElement responseJSON() throws IOException {
+			conn.setRequestProperty("Accept", "application/json");
+			boolean jsonCheck = false;
+			try {
+				JsonReader jr = new JsonReader(new InputStreamReader(getResponse()));
+				jr.setLenient(true);
+				jsonCheck = true;
+				jr.peek();
+				jsonCheck = false;
+				return JSON_ELEMENT.read(jr);
+			} catch (Exception x) {
+				if (x instanceof EOFException && jsonCheck) {
+					return JsonNull.INSTANCE;
+				}
+				throw x;
+			}
+		}
+
+		public String responseText() throws IOException {
+			conn.setRequestProperty("Accept", "text/plain; charset=UTF-8");
+			InputStream in = getResponse();
+			ByteArrayOutputStream bout = new ByteArrayOutputStream(Math.max(32, in.available()));
+			ByteStreams.copy(in, bout);
+			return bout.toString("UTF-8");
 		}
 	}
 }
