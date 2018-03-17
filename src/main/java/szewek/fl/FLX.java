da120451a4a92ba37afa37c380b1cb90a86c9416
Szewek <shevkpl@gmail.com>
2018-03-17 20:10:47 +0100

    Moved constants and capabilities to a Java class

--- src/main/java/szewek/fl/FLX.java
+++ src/main/java/szewek/fl/FLX.java
@@ -1,10 +1,20 @@
 package szewek.fl;
 
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.fml.common.SidedProxy;
+import szewek.fl.energy.IEnergy;
 import szewek.fl.proxy.FLProxy;
+import szewek.fl.taste.ILikesTaste;
 
 public final class FLX {
 
 	@SidedProxy(modId = R.FL_ID, serverSide = R.FL_PROXY_PKG, clientSide = R.FL_PROXY_PKG + "Client")
 	public static FLProxy PROXY = null;
+
+	@CapabilityInject(IEnergy.class)
+	public static Capability<IEnergy> ENERGY_CAP = null;
+
+	@CapabilityInject(ILikesTaste.class)
+	public static Capability<ILikesTaste> TASTE_CAP = null;
 }
---
+++ src/main/java/szewek/fl/R.java
@@ -0,0 +1,11 @@
+package szewek.fl;
+
+public final class R {
+	public static final String
+			FL_ID = "fl",
+			FL_VERSION = "${version}",
+			FL_PROXY_PKG = "szewek.fl.proxy.FLProxy",
+			FL_KEY = "pLQRD8nqLtXWnKf1pe2AxPCDgLqU6_hK3TjDJ1UEIUU";
+
+	public static final Boolean FL_DEBUG = java.lang.Boolean.getBoolean("szewek.fl.debug");
+}
